ecdea5d04c22e383f8858b3507b0738660cf588a
albertb22 <albertb22@hotmail.com>
2017-04-06 15:15:24 +0200

    Minified css

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ gulp.task('styles', function() {
   return gulp.src('src/scss/panor.scss')
 	.pipe(sass({ style: 'expanded' }))
 	.pipe(autoprefixer('safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-	//.pipe(csso())
+	.pipe(csso())
 	.pipe(rename('panor.min.css'))
 	.pipe(gulp.dest('dist'))
 	.pipe(notify({ message: 'Compiled scss files' }));
